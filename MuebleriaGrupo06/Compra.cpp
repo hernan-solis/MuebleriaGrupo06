ac774970cebb9f099e7857ff24d428e6c66eca28
nikiburgos <burgosveganicole@gmail.com>
2025-06-05 10:13:57 -0300

    Creada la clase Compra y compra.h
    TO DO: falta la clase fecha

---
+++ MuebleriaGrupo06/Compra.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "Compra.h"
+
+Compra::Compra() {
+    _idCompra = -1;
+    _idProveedor = -1;
+    _fechaCompra = // completar con el controlador de FECHA
+    _status = true;
+}
+
+Compra::Compra(int idCompra, int idProveedor, Fecha fechaCompra, bool status) {
+    _idCompra = idCompra;
+    _idProveedor = idProveedor;
+    _fechaCompra = fechaCompra;
+    _status = status;
+}
+
+int Compra::getIdCompra() {
+    return _idCompra;
+}
+
+int Compra::getIdProveedor() {
+    return _idProveedor;
+}
+
+Fecha Compra::getFechaCompra() {
+    return _fechaCompra;
+}
+
+bool Compra::getStatus() {
+    return _status;
+}
+
+void Compra::setIdCompra(int idCompra) {
+    _idCompra = idCompra;
+}
+
+void Compra::setIdProveedor(int idProveedor) {
+    _idProveedor = idProveedor;
+}
+
+void Compra::setFechaCompra(Fecha fechaCompra) {
+    _fechaCompra = fechaCompra;
+}
+
+void Compra::setStatus(bool status) {
+    _status = status;
+}
+
+std::string Compra::toCSV() {
+    return std::to_string(_idCompra) + "," +
+           std::to_string(_idProveedor) + "," +
+           _fechaCompra.toCSV() + "," +
+           std::to_string(_status);
+}
